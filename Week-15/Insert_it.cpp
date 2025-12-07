f457d6a97943439018e1409cc74e867333b1ce52
Amranhossan123 <mdimranh080@gmail.com>
2025-12-07 23:16:17 +0600

    Week-15

---
+++ Week-15/Insert_it.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    int m;
+    cin>>m;
+    vector<int>v1(m);
+    for(int i=0;i<m;i++){
+        cin>>v1[i];
+    }
+    int indx;
+    cin>>indx;
+    v.insert(v.begin()+indx,v1.begin(),v1.end());
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
